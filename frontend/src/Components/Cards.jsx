9eb8ca91b01f38a148b83af13872dc6b35255ad3
brucemorakane <brucemorakane74@gmail.com>
2023-05-31 16:17:40 +0200

    added everything is fine

--- frontend/src/Components/Cards.jsx
+++ frontend/src/Components/Cards.jsx
@@ -1,26 +1,59 @@
 import React from 'react'
 
 const Cards = () => {
-  return (
-    <div>
-        <div class="container">
-  <div class="row mt-5">
+    return (
+        <div>
 
-    <div class="preview-card">
-      <div class="preview-card__wrp">
-        <div class="preview-card__item">
-          <div class="preview-card__img">
-            <img src="https://images.unsplash.com/photo-1505852959938-8193566b2f8e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGZpc2htYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" alt=""/>
-          </div>
+            <div class="container">
+                <div class="row g-6">
+
+                    <div class="preview-card">
+                        <div class="preview-card__wrp">
+                            <div class="preview-card__item">
+                                <div class="preview-card__img">
+                                    <img src="https://images.unsplash.com/photo-1505852959938-8193566b2f8e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGZpc2htYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="preview-card">
+                        <div class="preview-card__wrp">
+                            <div class="preview-card__item">
+                                <div class="preview-card__img">
+                                    <img src="https://images.unsplash.com/photo-1505852959938-8193566b2f8e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGZpc2htYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="preview-card">
+                        <div class="preview-card__wrp">
+                            <div class="preview-card__item">
+                                <div class="preview-card__img">
+                                    <img src="https://images.unsplash.com/photo-1505852959938-8193566b2f8e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGZpc2htYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="preview-card">
+                        <div class="preview-card__wrp">
+                            <div class="preview-card__item">
+                                <div class="preview-card__img">
+                                    <img src="https://images.unsplash.com/photo-1505852959938-8193566b2f8e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGZpc2htYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
-
-      </div>
-    </div>
-
-  </div>
-</div>
-    </div>
-  )
+    )
 }
 
 export default Cards
