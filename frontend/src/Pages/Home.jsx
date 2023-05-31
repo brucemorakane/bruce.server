33d253369263de80cf01410aaf37c14286b316fc
brucemorakane <brucemorakane74@gmail.com>
2023-05-31 11:08:53 +0200

    added navbar and home

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Home
