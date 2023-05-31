373c67901f94e4defb84c62618c59eaaf10967b0
brucemorakane <brucemorakane74@gmail.com>
2023-05-31 14:04:40 +0200

    added import Slider added

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Slider from '../Components/Slider';
 
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
+        <Slider/>
     </div>
   )
 }
