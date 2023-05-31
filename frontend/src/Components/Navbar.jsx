d07122de24ef835d6515cd1b9de1e8da8d57b59d
brucemorakane <brucemorakane74@gmail.com>
2023-05-31 11:35:02 +0200

    deleted navbar-brand

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -5,7 +5,9 @@ const Navbar = () => {
     <div>
         <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <div className='logo'>
+      <img src="https://www.fishmantackle.co.za/wp-content/uploads/2019/09/Diamond-Small-200x98.jpg" width={80}></img>
+    </div>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
