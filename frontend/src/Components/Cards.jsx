154098cf9b9769bc18087489fb23bd596c407d95
brucemorakane <brucemorakane74@gmail.com>
2023-06-01 12:27:37 +0200

    added row align-items-center

--- frontend/src/Components/Cards.jsx
+++ frontend/src/Components/Cards.jsx
@@ -10,7 +10,7 @@ const Cards = () => {
                         <div class="preview-card__wrp">
                             <div class="preview-card__item">
                                 <div class="preview-card__img">
-                                    <img src="https://images.unsplash.com/photo-1597692493640-b70139c9e4ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={350} />
+                                    <img src="https://images.unsplash.com/photo-1597692493640-b70139c9e4ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={300} />
                                 </div><br />
                                 <a href="#" class="btn btn-dark">VIEWS MORE</a>
                             </div>
@@ -23,7 +23,7 @@ const Cards = () => {
                         <div class="preview-card__wrp">
                             <div class="preview-card__item">
                                 <div class="preview-card__img">
-                                    <img src="https://images.unsplash.com/photo-1584300005420-38486f627b07?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={350} />
+                                    <img src="https://images.unsplash.com/photo-1584300005420-38486f627b07?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={280} />
                                 </div>
                                 <a href="#" class="btn btn-dark">VIEWS MORE</a>
                             </div>
@@ -36,7 +36,7 @@ const Cards = () => {
                         <div class="preview-card__wrp">
                             <div class="preview-card__item">
                                 <div class="preview-card__img">
-                                    <img src="https://images.unsplash.com/photo-1548704087-b11dab0fbec0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={350} />
+                                    <img src="https://images.unsplash.com/photo-1548704087-b11dab0fbec0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={298} />
                                 </div><br />
                                 <a href="#" class="btn btn-dark">VIEWS MORE</a>
                             </div>
@@ -44,6 +44,8 @@ const Cards = () => {
                         </div>
                     </div>
                 </div>
+                <div class="row align-items-center mt-5">
+            </div>
             </div>
 
             <div class="row align-items-center mt-5">
@@ -52,7 +54,7 @@ const Cards = () => {
                     <div class="preview-card__wrp">
                         <div class="preview-card__item">
                             <div class="preview-card__img">
-                                <img src="https://images.unsplash.com/photo-1563557908-b7787229f123?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={350} />
+                                <img src="https://images.unsplash.com/photo-1563557908-b7787229f123?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={300} />
                             </div><br />
                             <a href="#" class="btn btn-dark">VIEWS MORE</a>
                         </div>
@@ -60,6 +62,8 @@ const Cards = () => {
                     </div>
                 </div>
             </div>
+
+            
 
 
         </div>
