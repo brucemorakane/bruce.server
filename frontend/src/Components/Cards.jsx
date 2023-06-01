75b6166b53100839f10f26b2a51100ca5d8ff87f
brucemorakane <brucemorakane74@gmail.com>
2023-06-01 11:42:44 +0200

    added card-body update

--- frontend/src/Components/Cards.jsx
+++ frontend/src/Components/Cards.jsx
@@ -1,27 +1,16 @@
 import React from 'react'
 
 const Cards = () => {
-    return (
-        <div>
-            <div class="container">
-                <div class="row g-4">
-~
-                    <div class="preview-card"><br />
-                        <div class="preview-card__wrp">
-                            <div class="preview-card__item">
-                                <div class="preview-card__img">
-                                    <img src="https://images.unsplash.com/photo-1597692493640-b70139c9e4ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={350} />
-                                </div><br />
-                                <a href="#" class="btn btn-dark">VIEWS MORE</a>
-                            </div>
 
-                        </div>
-                    </div>
-<br />
-~<div class="container text-center">
+<div class="container text-center">
   <div class="row align-items-start">
     <div class="col">
-      One of three columns
+    <div class="card" style="width: 18rem;">
+  <img src="https://images.unsplash.com/photo-1518732751612-2c0787ff5684?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzZ8fGZpc2glMjBmb29kc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="..."width={350}/>
+  <div class="card-body">
+  <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
     </div>
     <div class="col">
       One of three columns
@@ -54,67 +43,127 @@ const Cards = () => {
   </div>
 </div>
 
-                </div>
-            </div>
-            <div class="container">
-                <div class="row g-6">
-
-                    <div class="preview-card"><br />
-                        <div class="preview-card__wrp">
-                            <div class="preview-card__item">
-                                <div class="preview-card__img">
-                                    <img src="https://images.unsplash.com/photo-1584300005420-38486f627b07?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={350} />
-                                </div><br />
-                                <a href="#" class="btn btn-dark">VIEWS MORE</a>
-                            </div>
-
-                        </div>
-                    </div>
-
-~
-
-                </div>
-            </div>
-            <div class="container">
-                <div class="row g-6">
-
-                    <div class="preview-card"><br />
-                        <div class="preview-card__wrp">
-                            <div class="preview-card__item">
-                                <div class="preview-card__img">
-                                    <img src="https://images.unsplash.com/photo-1548704087-b11dab0fbec0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={350} />
-                                </div><br />
-                                <a href="#" class="btn btn-dark">VIEWS MORE</a>
-                            </div>
-
-                        </div>
-                    </div>
-~
-
-
-                </div>
-            </div>
-            <div class="container">
-                <div class="row g-6">
-
-                    <div class="preview-card"><br />
-                        <div class="preview-card__wrp">
-                            <div class="preview-card__item">
-                                <div class="preview-card__img">
-                                    <img src="https://images.unsplash.com/photo-1563557908-b7787229f123?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={350} />
-                                </div><br />
-                                <a href="#" class="btn btn-dark">VIEWS MORE</a>
-                            </div>
-
-                        </div>
-                    </div>
 
 
 
-                </div>
-            </div>
-        </div>
-    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     return (
+//         <div>
+//             <div class="container">
+//                 <div class="row g-4">
+// ~
+//                     <div class="preview-card"><br />
+//                         <div class="preview-card__wrp">
+//                             <div class="preview-card__item">
+//                                 <div class="preview-card__img">
+//                                     <img src="https://images.unsplash.com/photo-1597692493640-b70139c9e4ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={350} />
+//                                 </div><br />
+//                                 <a href="#" class="btn btn-dark">VIEWS MORE</a>
+//                             </div>
+
+//                         </div>
+//                     </div>
+
+// ~
+
+
+//                 </div>
+//             </div>
+//             <div class="container">
+//                 <div class="row g-6">
+
+//                     <div class="preview-card"><br />
+//                         <div class="preview-card__wrp">
+//                             <div class="preview-card__item">
+//                                 <div class="preview-card__img">
+//                                     <img src="https://images.unsplash.com/photo-1584300005420-38486f627b07?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={350} />
+//                                 </div><br />
+//                                 <a href="#" class="btn btn-dark">VIEWS MORE</a>
+//                             </div>
+
+//                         </div>
+//                     </div>
+
+// ~
+
+//                 </div>
+//             </div>
+//             <div class="container">
+//                 <div class="row g-6">
+
+//                     <div class="preview-card"><br />
+//                         <div class="preview-card__wrp">
+//                             <div class="preview-card__item">
+//                                 <div class="preview-card__img">
+//                                     <img src="https://images.unsplash.com/photo-1548704087-b11dab0fbec0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={350} />
+//                                 </div><br />
+//                                 <a href="#" class="btn btn-dark">VIEWS MORE</a>
+//                             </div>
+
+//                         </div>
+//                     </div>
+// ~
+
+
+//                 </div>
+//             </div>
+//             <div class="container">
+//                 <div class="row g-6">
+
+//                     <div class="preview-card"><br />
+//                         <div class="preview-card__wrp">
+//                             <div class="preview-card__item">
+//                                 <div class="preview-card__img">
+//                                     <img src="https://images.unsplash.com/photo-1563557908-b7787229f123?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={350} />
+//                                 </div><br />
+//                                 <a href="#" class="btn btn-dark">VIEWS MORE</a>
+//                             </div>
+
+//                         </div>
+//                     </div>
+
+
+
+//                 </div>
+//             </div>
+//         </div>
+//     )
 }
 
 export default Cards
