93790264da1ec36534e8c56e02abb3c38b32ea0e
brucemorakane <brucemorakane74@gmail.com>
2023-06-01 14:11:02 +0200

    added carousel-item active

--- frontend/src/Components/Slider.jsx
+++ frontend/src/Components/Slider.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
 const Slider = () => {
   return (
-    <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img data-lazy-src="https://i.ytimg.com/vi/0pRhBxOe3wo/hqdefault.jpg" alt="" width="480" height="360" data-pin-description="The 30 Best Freshwater Aquarium Fish Species" data-pin-title="The 30 Best Freshwater Aquarium Fish Species" data-ll-status="loaded" class="entered lazyloaded" src="https://i.ytimg.com/vi/0pRhBxOe3wo/hqdefault.jpg"></img>
-      </div>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://static1.olympus-lifescience.com/data/Image/Application/How-Old-Is-the-Fish/fig1.jpg?rev=39E3" class="d-block w-100" alt="..."/>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
+    <div class="carousel-item">
+      <img src="https://images.unsplash.com/photo-1451899516974-0b6a230204dc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDZ8fHNsaWRlJTIwZmlzaHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://images.unsplash.com/photo-1535443120147-89aef0b5327a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTl8fHNsaWRlJTIwZmlzaHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" class="d-block w-100" alt="..."/>
+    </div>
   </div>
+</div>
   )
 }
 
