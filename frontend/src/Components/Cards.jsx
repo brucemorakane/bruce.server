d4ae79b5c137bfa872a3c777bac2fc4a638c464a
brucemorakane <brucemorakane74@gmail.com>
2023-06-01 10:21:07 +0200

    added img src

--- frontend/src/Components/Cards.jsx
+++ frontend/src/Components/Cards.jsx
@@ -6,7 +6,31 @@ const Cards = () => {
             <div class="container">
                 <div class="row g-6">
 
-                    <div class="preview-card">
+                    <div class="preview-card"><br />
+                        <div class="preview-card__wrp">
+                            <div class="preview-card__item">
+                                <div class="preview-card__img">
+                                    <img src="https://images.unsplash.com/photo-1597692493640-b70139c9e4ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={350}/>
+                                </div><br />
+                                <a href="#" class="btn btn-dark">VIEWS MORE</a>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="preview-card"><br />
+                        <div class="preview-card__wrp">
+                            <div class="preview-card__item">
+                                <div class="preview-card__img">
+                                    <img src="https://images.unsplash.com/photo-1601314002592-b8734bca6604?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZmlzaCUyMGZvb2R8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" alt=""width={350} />
+                                </div>
+                                <a href="#" class="btn btn-dark">VIEWS MORE</a>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="preview-card"><br />
                         <div class="preview-card__wrp">
                             <div class="preview-card__item">
                                 <div class="preview-card__img">
@@ -16,29 +40,7 @@ const Cards = () => {
 
                         </div>
                     </div>
-
-                    <div class="preview-card">
-                        <div class="preview-card__wrp">
-                            <div class="preview-card__item">
-                                <div class="preview-card__img">
-                                    <img src="https://images.unsplash.com/photo-1505852959938-8193566b2f8e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGZpc2htYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" alt="" />
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <div class="preview-card">
-                        <div class="preview-card__wrp">
-                            <div class="preview-card__item">
-                                <div class="preview-card__img">
-                                    <img src="https://images.unsplash.com/photo-1505852959938-8193566b2f8e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGZpc2htYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" alt="" />
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div class="preview-card">
+                    <div class="preview-card"><br />
                         <div class="preview-card__wrp">
                             <div class="preview-card__item">
                                 <div class="preview-card__img">
