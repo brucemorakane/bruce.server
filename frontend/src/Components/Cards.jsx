7e6227f423fa81de679f1740f26d46fe84a6be0d
brucemorakane <brucemorakane74@gmail.com>
2023-06-01 15:24:43 +0200

    deleted col

--- frontend/src/Components/Cards.jsx
+++ frontend/src/Components/Cards.jsx
@@ -60,19 +60,6 @@ const Cards = () => {
                 </div>
                 <div class="row align-items-center mt-5">
             </div>
-            <div class="col">
-                    <div class="preview-card">
-                        <div class="preview-card__wrp">
-                            <div class="preview-card__item">
-                                <div class="preview-card__img">
-                                    <img src="https://images.unsplash.com/photo-1597692493640-b70139c9e4ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={300} />
-                                </div><br />
-                                <a href="#" class="btn btn-dark">VIEWS MORE</a>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
             </div>
            
 
