a88566c79816f472887dd0e26774f51dcb4a088b
brucemorakane <brucemorakane74@gmail.com>
2023-06-05 09:31:27 +0200

    deleted col

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -17,35 +17,6 @@ const Footer = () => {
 
 <div className="col-xs-12 col-sm-6 col-md-3">
 <div className="widget no-box">
-<h5 className="widget-title">Quick Links<span></span></h5>
-<ul className="thumbnail-widget">
-<li>
-<div className="thumb-content"><a href="#.">Get Started</a></div> 
-</li>
-<li>
-<div className="thumb-content"><a href="#.">Top Leaders</a></div> 
-</li>
-<li>
-<div className="thumb-content"><a href="#.">Success Stories</a></div> 
-</li>
-<li>
-<div className="thumb-content"><a href="#.">Event/Tickets</a></div> 
-</li>
-<li>
-<div className="thumb-content"><a href="#.">News</a></div>  
-</li>
-<li>
-<div className="thumb-content"><a href="#.">Lifestyle</a></div> 
-</li>
-<li>
-<div className="thumb-content"><a href="#.">About</a></div> 
-</li>
-</ul>
-</div>
-</div>
-
-<div className="col-xs-12 col-sm-6 col-md-3">
-<div className="widget no-box">
 <h5 className="widget-title">Get Started<span></span></h5>
 <p>Get access to your full Training and Marketing Suite.</p>
 <a className="btn" href="https://bit.ly/3m9avif" target="_blank">Subscribe Now</a>
