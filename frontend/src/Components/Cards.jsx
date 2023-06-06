b13bcf3c342ecf32c896bccb2b57380dac8131d8
brucemorakane <brucemorakane74@gmail.com>
2023-06-06 11:44:57 +0200

    added preview-card

--- frontend/src/Components/Cards.jsx
+++ frontend/src/Components/Cards.jsx
@@ -31,13 +31,13 @@ const Cards = () => {
                         </div>
                     </div>
                 </div>
-                
+
                 <div class="col">
                     <div class="preview-card">
                         <div class="preview-card__wrp">
-                            <div class="preview-card__item">
+                        <div class="preview-card__item">
                                 <div class="preview-card__img">
-                                    <img src="https://images.unsplash.com/photo-1584300005420-38486f627b07?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={280} />
+                                    <img src="https://images.unsplash.com/photo-1597692493640-b70139c9e4ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={300} />
                                 </div><br />
                                 <a href="#" class="btn btn-dark">VIEWS MORE</a>
                             </div>
@@ -48,9 +48,9 @@ const Cards = () => {
                 <div class="col">
                     <div class="preview-card">
                         <div class="preview-card__wrp">
-                            <div class="preview-card__item">
+                        <div class="preview-card__item">
                                 <div class="preview-card__img">
-                                    <img src="https://images.unsplash.com/photo-1548704087-b11dab0fbec0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={298} />
+                                    <img src="https://images.unsplash.com/photo-1597692493640-b70139c9e4ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={300} />
                                 </div><br />
                                 <a href="#" class="btn btn-dark">VIEWS MORE</a>
                             </div>
@@ -59,9 +59,9 @@ const Cards = () => {
                     </div>
                 </div>
                 <div class="row align-items-center mt-5">
+                </div>
             </div>
-            </div>
-           
+
 
             {/* <div class="row align-items-center mt-5">
 
@@ -77,9 +77,9 @@ const Cards = () => {
                     </div>
                 </div>
             </div> */}
-            
 
-            
+
+
 
 
         </div>
