81239e02ac75a07446350dac309ffc029e0e9ab1
brucemorakane <brucemorakane74@gmail.com>
2023-06-06 10:51:55 +0200

    added update row

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -3,12 +3,14 @@ const Footer = () => {
   return (
     <div class="mt-5 pt-5 pb-5 footer">
       <div class="container">
+        <div className='row'>
         <div class="row">
           <div class="col-lg-5 col-xs-12 about-company">
             <h2>Heading</h2>
             <p class="pr-5 text-dark-50">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac ante mollis quam tristique convallis </p>
             <p><a href="#"><i class="fa fa-facebook-square mr-1"></i></a><a href="#"><i class="fa fa-linkedin-square"></i></a></p>
           </div>
+        </div>
           <div class="col-lg-3 col-xs-12 links">
             <h4 class="mt-lg-0 mt-sm-3">Links</h4>
             <div className='social_media'>
