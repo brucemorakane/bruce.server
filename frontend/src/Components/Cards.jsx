039e7dab5b44a5803a72e0ad3b21b18898ca93b1
brucemorakane <brucemorakane74@gmail.com>
2023-06-06 11:48:07 +0200

    deleted preview-card

--- frontend/src/Components/Cards.jsx
+++ frontend/src/Components/Cards.jsx
@@ -58,26 +58,10 @@ const Cards = () => {
                         </div>
                     </div>
                 </div>
+                
                 <div class="row align-items-center mt-5">
                 </div>
             </div>
-
-
-            {/* <div class="row align-items-center mt-5">
-
-                <div class="preview-card">
-                    <div class="preview-card__wrp">
-                        <div class="preview-card__item">
-                            <div class="preview-card__img">
-                                <img src="https://images.unsplash.com/photo-1563557908-b7787229f123?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={300} />
-                            </div><br />
-                            <a href="#" class="btn btn-dark">VIEWS MORE</a>
-                        </div>
-
-                    </div>
-                </div>
-            </div> */}
-
 
 
 
