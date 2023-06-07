4500181484956ab603891be256769f5fb3b663ea
brucemorakane <brucemorakane74@gmail.com>
2023-06-07 13:48:58 +0200

    added update

--- frontend/src/Components/Slider.jsx
+++ frontend/src/Components/Slider.jsx
@@ -4,7 +4,7 @@ const Slider = () => {
     <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
   <div className="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://static1.olympus-lifescience.com/data/Image/Application/How-Old-Is-the-Fish/fig1.jpg?rev=39E3" class="d-block w-100" alt="..."height={700}/>
+      <img src="https://static1.olympus-lifescience.com/data/Image/Application/How-Old-Is-the-Fish/fig1.jpg?rev=39E3" class="d-block w-100" alt="..."height={715}/>
     </div>
   </div>
 </div>
