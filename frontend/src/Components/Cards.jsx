759ddd8c59606a5e40657288262d315d1ef0a31d
brucemorakane <brucemorakane74@gmail.com>
2023-06-07 08:24:01 +0200

    added img src

--- frontend/src/Components/Cards.jsx
+++ frontend/src/Components/Cards.jsx
@@ -10,7 +10,7 @@ const Cards = () => {
                         <div class="preview-card__wrp">
                             <div class="preview-card__item">
                                 <div class="preview-card__img">
-                                    <img src="https://images.unsplash.com/photo-1597692493640-b70139c9e4ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={300} />
+                                    <img src="https://media.istockphoto.com/id/953583152/photo/slave-raw-fish-on-wooden-cutting-board-on-dark-background-lemon-and-pepper-mix.webp?b=1&s=170667a&w=0&k=20&c=lZyKf2g20G6NLHcksSuwROWMk12QSWWQ1AJZ1RhuJFw=" alt="" width={300} />
                                 </div><br />
                                 <a href="#" class="btn btn-dark">VIEWS MORE</a>
                             </div>
@@ -23,7 +23,7 @@ const Cards = () => {
                         <div class="preview-card__wrp">
                             <div class="preview-card__item">
                                 <div class="preview-card__img">
-                                    <img src="https://images.unsplash.com/photo-1597692493640-b70139c9e4ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={300} />
+                                    <img src="https://media.istockphoto.com/id/953582076/photo/slave-raw-fish-on-wooden-cutting-board-on-dark-background.webp?b=1&s=170667a&w=0&k=20&c=atPmivHBlL6wTGASJdJMHTm9FwlaiQngwpHCkGpLvig=" alt="" width={300} />
                                 </div><br />
                                 <a href="#" class="btn btn-dark">VIEWS MORE</a>
                             </div>
@@ -37,7 +37,7 @@ const Cards = () => {
                         <div class="preview-card__wrp">
                             <div class="preview-card__item">
                                 <div class="preview-card__img">
-                                    <img src="https://images.unsplash.com/photo-1597692493640-b70139c9e4ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={300} />
+                                    <img src="https://images.unsplash.com/photo-1498654200943-1088dd4438ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGZpc2h8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" alt="" width={300} />
                                 </div><br />
                                 <a href="#" class="btn btn-dark">VIEWS MORE</a>
                             </div>
@@ -50,7 +50,7 @@ const Cards = () => {
                         <div class="preview-card__wrp">
                             <div class="preview-card__item">
                                 <div class="preview-card__img">
-                                    <img src="https://images.unsplash.com/photo-1597692493640-b70139c9e4ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={300} />
+                                    <img src="https://images.unsplash.com/photo-1515669097368-22e68427d265?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8ZmlzaCUyMGZvb2RzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={300} />
                                 </div><br />
                                 <a href="#" class="btn btn-dark">VIEWS MORE</a>
                             </div>
@@ -69,7 +69,7 @@ const Cards = () => {
                         <div class="preview-card__wrp">
                             <div class="preview-card__item">
                                 <div class="preview-card__img">
-                                    <img src="https://images.unsplash.com/photo-1597692493640-b70139c9e4ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" width={300} />
+                                    <img src="https://images.unsplash.com/photo-1499125562588-29fb8a56b5d5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fGZpc2glMjBmb29kc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" width={300} />
                                 </div><br />
                                 <a href="#" class="btn btn-dark">VIEWS MORE</a>
                             </div>
